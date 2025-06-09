5853785ff07c25aecb3b3ca3573550e657ce3af3
Simon Vargas <sjvargas01@gmail.com>
2025-06-09 17:22:34 -0400

    first commit

---
+++ HelloWorld.cob
@@ -0,0 +1,13 @@
+000100 identification division.
+000200 PROGRAM-ID. HelloWorld.
+000300 author. Simon Vargas 
+000400 date-written. 2025-06-09.
+000500 data division.
+000600 procedure division.
+000700
+000800 program-begin.
+000900 display "Hello, World!".
+001000 stop run.
+001100 program-end.
+001200
+001300 end program HelloWorld.
